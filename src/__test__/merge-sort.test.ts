2714493cdc62c57b42ec6e740b242f473eb5fa90
tur1ng <mohammadali.ap.2000@gmail.com>
2021-08-14 23:46:41 +0430

    merge sort test

---
+++ src/__test__/merge-sort.test.ts
@@ -0,0 +1,20 @@
+import { mergeSort } from '../MergeSort';
+
+describe('Basic functionality', () => {
+  it('sort number array', () => {
+    const sortedArr = mergeSort([23, 345, 5467, 12, 2345, 9852]);
+    expect(sortedArr).toEqual([12, 23, 345, 2345, 5467, 9852]);
+  });
+});
+
+describe('Bad inputs', () => {
+  it('array with one element should return itself', () => {
+    const sortedArr = mergeSort([0]);
+    expect(sortedArr).toEqual([0]);
+  });
+
+  it('array with all elements the same should return itself', () => {
+    const sortedArr = mergeSort([2, 2, 2, 2]);
+    expect(sortedArr).toEqual([2, 2, 2, 2]);
+  });
+});
