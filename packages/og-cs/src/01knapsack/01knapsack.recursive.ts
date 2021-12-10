65c64c711a9cea76cf4510a39f7eda139c8eeff1
tur1ng <mohammadali.ap.2000@gmail.com>
2021-12-10 21:34:54 +0330

    0/1 knapsack recursive

---
+++ packages/og-cs/src/01knapsack/01knapsack.recursive.ts
@@ -0,0 +1,19 @@
+export const knapsackRecursive = (
+  profits: Array<number>,
+  weights: Array<number>,
+  capacity: number,
+  currentIndex: number
+): number => {
+  if (capacity === 0 || currentIndex === 0) return 0;
+
+  if (weights[currentIndex - 1] > capacity) {
+    return knapsackRecursive(profits, weights, capacity, currentIndex - 1);
+  } else {
+    const profit1 = knapsackRecursive(profits, weights, capacity, currentIndex - 1);
+    const profit2 =
+      profits[currentIndex - 1] +
+      knapsackRecursive(profits, weights, capacity - weights[currentIndex - 1], currentIndex - 1);
+
+    return Math.max(profit1, profit2);
+  }
+};
