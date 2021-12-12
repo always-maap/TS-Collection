c85565140ab038b94d8156389b40b2bd67f8ec3a
tur1ng <mohammadali.ap.2000@gmail.com>
2021-12-12 08:31:37 +0330

    test(core): quick sort

---
+++ packages/core/test/quick-sort.test.ts
@@ -0,0 +1,28 @@
+import { quickSort } from '../src';
+
+describe('Basic functionality', () => {
+  it('sort the unsorted arr', () => {
+    const arr = [5, 3, 7, 1, 0];
+    expect(quickSort(arr)).toEqual([0, 1, 3, 5, 7]);
+  });
+
+  it('sort the unsorted arr with duplicate inputs', () => {
+    const arr = [-1, -1, 0, 5, 12];
+    expect(quickSort(arr)).toEqual([-1, -1, 0, 5, 12]);
+  });
+});
+
+describe('bad inputs', () => {
+  it('return empty arr when arr is empty', () => {
+    expect(quickSort([])).toEqual([]);
+  });
+
+  it('return same arr whenever arr in sorted', () => {
+    const arr = [-5, -2, 0, 2, 5];
+    expect(quickSort(arr)).toEqual([-5, -2, 0, 2, 5]);
+  });
+
+  it('return same item whenever arr length is 1', () => {
+    expect(quickSort([1])).toEqual([1]);
+  });
+});
