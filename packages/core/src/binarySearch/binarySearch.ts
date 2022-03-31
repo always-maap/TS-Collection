44227401c46cd6088ff0b535f831d28eb8127e9f
Alireza Jafartash <alirezaopmc@gmail.com>
2022-03-31 14:25:03 +0430

    refactor: binary search returns the first occurrence now

--- packages/core/src/binarySearch/binarySearch.ts
+++ packages/core/src/binarySearch/binarySearch.ts
@@ -9,19 +9,20 @@ export const binarySearch = <T>(arr: Array<T>, target: T, compareFunction?: Comp
   let start = 0,
     end = arr.length - 1;
 
+  let first = -1;
+
   while (start <= end) {
-    const middle = Math.floor((start + end) / 2);
+    const middle = start + Math.floor((end - start) / 2);
 
     if (comparator.equal(arr[middle], target)) {
-      return middle;
-    }
-
-    if (comparator.greaterThan(arr[middle], target)) {
+      first = middle;
+      end = middle - 1;
+    } else if (comparator.greaterThan(arr[middle], target)) {
       end = middle - 1;
     } else {
       start = middle + 1;
     }
   }
 
-  return -1;
+  return first;
 };
